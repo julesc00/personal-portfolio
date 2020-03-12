cc17f31753d5f31e55267f9f3690cf2d2cbaf65f
juliobriones <julesc00@hotmail.com>
2020-03-11 22:08:17 -0600

    Adding picture and resume files.

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Post
 
 def all_blogs(request):
-  posts = Post.objects.all()
+  posts = Post.objects.order_by('-date_created')[:5]
   return render(request, "blog/all_blogs.html", {"posts": posts})
